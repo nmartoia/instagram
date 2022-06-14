3aea47d2991c059562182253eb910dfa399d08d6
nmartoia <nmartoia@edenschool.fr>
2022-06-14 15:32:34 +0200

    la bar utilitaire saffiche

--- js/app.js
+++ js/app.js
@@ -14,7 +14,6 @@ for (let i = 0; i < 10; i++) {
         like:getRandomInt(1000)
     }
     tmb.push(obj)
-    console.log(obj.isBooked==true)
     const li=document.createElement('li')
     li.className='post'
     const ul2=document.createElement('ul')
@@ -36,7 +35,42 @@ for (let i = 0; i < 10; i++) {
     imgU.className='imgU'
     imgU.src=obj.avatarURL
     imgU.alt=obj.avatarURL
-    li1.appendChild(imgU)
+    const pseudo=document.createElement('h4')
+    pseudo.textContent=obj.pseudo
+    li4.appendChild(imgU)
+    li5.appendChild(pseudo)
+    if(obj.isSponsored==true){
+        const sponsored=document.createElement('p')
+        sponsored.className='sponsored'
+        sponsored.textContent='sponsored'
+        li5.appendChild(sponsored)
+    }
+    li5.className='li5'
+    li1.appendChild(li4)
+    li1.appendChild(li5)
+    const li6 =document.createElement('li')
+    const coeur= document.createElement('i')
+    coeur.className='fa-regular fa-heart'
+    coeur.addEventListener('click',()=>{
+        if(coeur.className==='fa-regular fa-heart'){
+            coeur.className='fa-solid fa-heart'
+        }
+        else{
+            coeur.className='fa-regular fa-heart'
+        }
+    })
+    const comment= document.createElement('i')
+    comment.className="fa-solid fa-comment"
+    const partager= document.createElement('i')
+    partager.className="fa-solid fa-paper-plane"
+    const like =document.createElement('p')
+    like.className='like'
+    like.textContent=obj.like+' like'
+    li6.appendChild(coeur)
+    li6.appendChild(comment)
+    li6.appendChild(partager)
+    li6.appendChild(like)
+    li3.appendChild(li6)
     ul.appendChild(li)
     console.log(li)
 }
